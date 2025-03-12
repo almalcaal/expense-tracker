63975ecaec0349f4f0b46837d977b7f0e9b8df96
Aldrin Caalim <aldrinmcaalim@gmail.com>
2025-03-12 17:30:16 -0400

    ui finished

--- backend/typeDefs/transaction.typeDef.js
+++ backend/typeDefs/transaction.typeDef.js
@@ -20,7 +20,6 @@ type Mutation {
     createTransaction(input: CreateTransactionInput!): Transaction!
     updateTransaction(input: UpdateTransactionInput!): Transaction!
     deleteTransaction(transactionId: ID!): Transaction!
-    
 }
 
 input CreateTransactionInput {
